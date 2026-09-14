1e68e414977717947f2870bc11b2731a12e1b18b
Electra <electrachan1027@gmail.com>
2026-09-14 14:25:44 -0400

    Added Main class

---
+++ src/lab1/Main.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 50) {
+            FizzBuzz.doFizzBuzz(i);
+            i++;
+        }
+    }
+}
